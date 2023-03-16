5518b80de43c8ef94efd2194e5b8f2a1aef4fec8
jobeth <13076652356@163.com>
2023-03-17 01:08:42 +0800

    提交

---
+++ gateway-service/src/main/java/com/jobeth/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.jobeth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * Created with IntelliJ IDEA in 2023/3/17 0:24
+ * Description: 类
+ * <p>
+ *
+ * @author Jobeth
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
